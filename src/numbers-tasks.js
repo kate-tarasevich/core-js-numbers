01a72f57cf2720344b37cf0fa5de91179ed47656
Katsiaryna Tarasevich <69838564+kate-tarasevich@users.noreply.github.com>
2023-11-17 18:58:02 +0100

    feat: done isPowerOfTwo

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -317,8 +317,11 @@ function getSumOfDigits(/* num */) {
  *   16  => true
  *   15  => false
  */
-function isPowerOfTwo(/* num */) {
-  throw new Error('Not implemented');
+function isPowerOfTwo(num) {
+  let result;
+  if (Number.isInteger(Math.log2(num))) result = 1;
+  else result = 0;
+  return Boolean(result);
 }
 
 /**
